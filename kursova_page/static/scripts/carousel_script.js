89a73c9923bff509e6a94e55eaf570e164e1014f
ruslantuz <ruslantuz10@gmail.com>
2023-11-27 10:22:11 +0200

    Swiper improvement

--- kursova_page/static/scripts/carousel_script.js
+++ kursova_page/static/scripts/carousel_script.js
@@ -1,50 +1,11 @@
-
-
-function sidebarWidth(){
-  var winWidth = $(window).width();
-  if(winWidth > 1120){
-    $('.destinations .swiper').removeClass('mySwiper-mob').addClass('mySwiper');
-    // $('.destinations .swiper').addClass('mySwiper');
-    $('.special-offers .swiper').removeClass('offerSwiper-mob').addClass('offerSwiper');
-    // $('.special-offers .swiper').addClass('offerSwiper');
-  } else {
-    $('.destinations .swiper').removeClass('mySwiper').addClass('mySwiper-mob');
-    // $('.destinations .swiper').addClass('mySwiper-mob');
-    $('.special-offers .swiper').removeClass('offerSwiper').addClass('offerSwiper-mob');
-    // $('.special-offers .swiper').addClass('offerSwiper-mob');
-  };
-};
-
-sidebarWidth();
-window.onresize = sidebarWidth;
-$(document).resize(function(){
-  sidebarWidth();
-});
-
-// TEST DYNAMIC CHANGE:
-// 
-// function sidebarWidth(){
-//   var winWidth = $(window).width();
-//   if(winWidth > 1120){
-//     console.log("more")
-//     $('.destinations .swiper').removeClass('mySwiper-mob');
-//     $('.destinations .swiper').addClass('mySwiper');
-//     $('.special-offers .swiper').removeClass('offerSwiper-mob');
-//     $('.special-offers .swiper').addClass('offerSwiper');
-//   } else {
-//     console.log("less")
-//     $('.destinations .swiper').removeClass('mySwiper');
-//     $('.destinations .swiper').addClass('mySwiper-mob');
-//     $('.special-offers .swiper').removeClass('offerSwiper');
-//     $('.special-offers .swiper').addClass('offerSwiper-mob');
-//   };
-// };
-
-
-// Destinations:
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+var swiperMob = new Swiper(".mySwiper", {
+  slidesPerView: 1,
   spaceBetween: 32,
+  breakpoints: {
+    1122: {
+      slidesPerView: 3
+    }
+  },
   grabCursor: true,
   navigation: {
     nextEl: ".destinations .arrow-right",
@@ -53,30 +14,14 @@ var swiper = new Swiper(".mySwiper", {
 
 });
 
-var swiperMob = new Swiper(".mySwiper-mob", {
+var offerSwiperMob = new Swiper(".offerSwiper", {
   slidesPerView: 1,
   spaceBetween: 32,
-  grabCursor: true,
-  navigation: {
-    nextEl: ".destinations .arrow-right",
-    prevEl: ".destinations .arrow-left",
+  breakpoints: {
+    1122: {
+      slidesPerView: 3
+    }
   },
-
-});
-
-// Offers:
-var offerSwiper = new Swiper(".offerSwiper", {
-  slidesPerView: 3,
-  spaceBetween: 32,
-  grabCursor: false,
-  navigation: {
-    nextEl: ".special-offers .arrow-right",
-    prevEl: ".special-offers .arrow-left",
-  },
-});
-var offerSwiperMob = new Swiper(".offerSwiper-mob", {
-  slidesPerView: 1,
-  spaceBetween: 32,
   grabCursor: false,
   navigation: {
     nextEl: ".special-offers .arrow-right",
