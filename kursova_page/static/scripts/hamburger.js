05fbf739ec403aa442029919a080b8907f291312
ruslantuz <ruslantuz10@gmail.com>
2023-12-04 09:32:48 +0200

    fixed blocks + links

--- kursova_page/static/scripts/hamburger.js
+++ kursova_page/static/scripts/hamburger.js
@@ -5,7 +5,6 @@ const header = document.getElementById('headerId')
 
 iconMenu.addEventListener('click', ()=> {
 	navigation.classList.toggle('hide');
-    // heroWrap.classList.toggle('menu-space');
     header.classList.toggle('burger-clicked');
     iconMenu.classList.toggle("active");
 })
