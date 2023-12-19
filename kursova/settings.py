710a8a05c7d8a317d60692596b4866c7fb44b7f8
ruslantuz <ruslantuz10@gmail.com>
2023-12-19 10:13:43 +0200

    testing whitenoise

--- kursova/settings.py
+++ kursova/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
