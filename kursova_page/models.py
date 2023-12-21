cdccbe8a003f3142f383295af05cb77c3ec69271
ruslantuz <ruslantuz10@gmail.com>
2023-12-21 19:23:25 +0200

    migrations adjustment

--- kursova_page/models.py
+++ kursova_page/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Offers(models.Model):
     location = models.CharField(max_length=100)
-    rating = models.IntegerField(max_length=1)
+    rating = models.IntegerField()
     description = models.TextField(max_length=400)
     pricing = models.FloatField(max_length=10)
     offer_image = models.ImageField(null = True, blank=True, upload_to="images/")
